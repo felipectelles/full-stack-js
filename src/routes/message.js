44f8dca33b50d49577b551c1318d0d0a181dd9fe
felipecorreatelles@gmail.com <felipecorreatelles@gmail.com>
2019-11-11 01:59:22 -0300

    setting up back rest

---
+++ src/routes/message.js
@@ -0,0 +1,40 @@
+import uuidv4 from 'uuid/v4';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const messages = await req.context.models.Message.find();
+    return res.send(messages);
+});
+
+router.get('/:messageId', async (req, res) => {
+    const message = await req.context.models.Message.findById(
+        req.params.messageId,
+    );
+    return res.send(message);
+});
+
+router.post('/', async (req, res) => {
+    const message = req.context.models.Message.messageIdcreate({
+        text: req.body.text,
+        user: req.context.me.id,
+    });
+    
+    return res.send(message);
+});
+
+router.delete('/:messageId', async (req, res) => {
+    const message = await req.context.models.Message.findById(
+        req.params.messageId,
+    );
+    
+    let result = null;
+    if (message) {
+        result = await message.remove();
+    }
+
+    return res.send(result);
+});
+
+export default router;
